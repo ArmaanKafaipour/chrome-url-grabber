b5f13b6069fd09641482f5503ea13714045931fc
ArmaanKafaipour <k_armaan@yahoo.com>
2021-09-21 22:00:48 -0700

    Add project files.

---
+++ UrlGrabber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace firefoxURLgrabber
+{
+    public static class UrlGrabber
+    {
+        public static string GetUrl()
+        {
+            Process[] procsChrome = Process.GetProcessesByName("chrome");
+            foreach (Process chrome in procsChrome)
+            {
+                if (chrome.MainWindowHandle == IntPtr.Zero)
+                    continue;
+
+                AutomationElement element = AutomationElement.FromHandle(chrome.MainWindowHandle);
+                if (element == null)
+                {
+                    return null;
+                }
+                Condition conditions = new AndCondition(
+                    new PropertyCondition(AutomationElement.ProcessIdProperty, chrome.Id),
+                    new PropertyCondition(AutomationElement.IsControlElementProperty, true),
+                    new PropertyCondition(AutomationElement.IsContentElementProperty, true),
+                    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+
+                AutomationElement elementx = element.FindFirst(TreeScope.Descendants, conditions);
+                return ((ValuePattern)elementx.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
+            }
+
+            return null;
+
+        }
+    }
+}
+
+    
